5b68be4ba2695cd01b4ef5d1e160390626c41436
C0mbed <matt@farncombe.co>
2021-12-17 05:44:33 -0700

    complete

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code here!
+
+let removeMain = document.getElementById('main');
+removeMain.remove();
+
+let newHeader = document.createElement('H1');
+newHeader.setAttribute('id', 'victory');
+let headerText = document.createTextNode("Matt is the champion");
+newHeader.appendChild(headerText);
